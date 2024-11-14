7535a829df7944d184d57bafd62ee68faee83be4
Cibli Cibni <ciblicibni@gmail.com>
2024-11-14 08:38:17 +0100

    Version 2

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 consoole.log("config.js");
 console.log("version1");
+console.log("cconfig");
+console.log("version2");
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log("index.js");
 console.log("version1");
+onsole.log("vestion2");
